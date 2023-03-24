67c619f14fe4c8c6ce0d45b1981841b4a3e84760
JaspersJas1743 <lesha.smirnov2019@mail.ru>
2023-03-24 22:27:46 +0300

    Добавьте файлы проекта.

---
+++ ElectronicJournal/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using ElectronicJournal.Models;
+using System;
+using System.Timers;
+using System.Windows;
+
+namespace ElectronicJournal
+{
+	public partial class MainWindow : Window
+	{
+		private TimeModel _time = new TimeModel();
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			Time.DataContext = _time;
+			Timer timer = new Timer(interval: 1000);
+			timer.Elapsed += (sender, e) => _time.Time = DateTime.Now;
+			timer.Start();
+		}
+
+		private void OnBackButtonClick(object sender, RoutedEventArgs e)
+		{
+		}
+
+		private void OnWindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
+			=> e.Cancel = !MessageBox.Show(
+					messageBoxText: "Вы уверены, что хотите закрыть приложение?",
+					caption: "Закрыть",
+					button: MessageBoxButton.YesNo,
+					icon: MessageBoxImage.Warning
+				).Equals(obj: MessageBoxResult.Yes);
+	}
+}
